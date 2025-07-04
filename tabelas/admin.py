ce6ea0374a07ae6a5c275208605dad203e1b81a4
kellyalvesp <zkellypatriciaalves14@gmail.com>
2025-07-04 12:18:49 -0300

    Adicionando a tabela receita medicamento #5

--- tabelas/admin.py
+++ tabelas/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import Especialidade
 from .models import Medico
 from .models import Consulta
-from .models import Receitas
+from .models import Receita
+from .models import ReceitaMedicamento
 
 admin.site.register(Especialidade)
 admin.site.register(Medico)
 admin.site.register(Consulta)
-admin.site.register(Receitas)
+admin.site.register(Receita)
+admin.site.register(ReceitaMedicamento)
