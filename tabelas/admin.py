44d8cc7a1847b69601f6ad97541a57068c6f81bf
kellyalvesp <zkellypatriciaalves14@gmail.com>
2025-07-04 13:03:39 -0300

    Adicionando tabela paciente #7

--- tabelas/admin.py
+++ tabelas/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Especialidade
 from .models import Medico
 from .models import Consulta
+from .models import Paciente
 from .models import Receita
 from .models import ReceitaMedicamento
 from .models import Medicamento
@@ -9,6 +10,7 @@ from .models import Medicamento
 admin.site.register(Especialidade)
 admin.site.register(Medico)
 admin.site.register(Consulta)
+admin.site.register(Paciente)
 admin.site.register(Receita)
 admin.site.register(ReceitaMedicamento)
 admin.site.register(Medicamento)
